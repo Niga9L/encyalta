191bc1dfdb87f227ab19d5f8ec71e469809955bd
Niga9L <lyaginvanya@gmail.com>
2021-04-09 16:37:26 +0300

    Разброс сущности User и Auth

--- src/jwt-passport/interfaces/requestWithUser.interface.ts
+++ src/jwt-passport/interfaces/requestWithUser.interface.ts
@@ -1,8 +1,12 @@
 import { Request } from 'express';
 import User from '../../users/entities/user.entity';
 
+type UserRequest = {
+  userId: number;
+} & User;
+
 interface RequestWithUser extends Request {
-  user: User & { userId: number };
+  user: UserRequest;
 }
 
 export default RequestWithUser;
